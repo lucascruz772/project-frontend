fa94016db5be4b3a4c595e3612999fb58b9b268b
Lucas Miranda <lucascruz772@gmail.com>
2025-03-31 21:32:14 -0300

    ok

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/project-frontend/",
   css: {
     modules: {
       localsConvention: "camelCase", // Garante que as classes sejam acessíveis como camelCase (ex.: styles.taskList)
